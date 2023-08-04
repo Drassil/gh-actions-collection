b077edc2e23a8a5e4328c2f6b2aff69e8dfc5edb
Yehonal <yehonal.azeroth@gmail.com>
2023-08-04 20:17:23 +0000

    fix import minimatch

--- changed-files/index.js
+++ changed-files/index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const exec = require("@actions/exec");
-const minimatch = require("minimatch");
+const { minimatch } = require("minimatch");
 
 async function run() {
   try {
